c1abb25b86687f86328b91cde2ae688644bdd41c
Aarno Aukia <aarno.aukia@vshn.ch>
2017-02-12 18:35:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Short Python Cherrypy Hello world example
---
+++ app.py
@@ -0,0 +1,13 @@
+import cherrypy
+import os
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        message = "Hello World!\n\n"
+        for key in os.environ.keys():
+            message += '{0}: {1}\n'.format(key, os.environ.get(key))
+        return message
+
+if __name__ == '__main__':
+   cherrypy.quickstart(Root(), '/')
